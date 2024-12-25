cb242dd232c2a64d3fdcf491373047f47f578807
petmoe <petter.moe@brilliant.no>
2024-12-25 17:31:00 +0100

    make email lookup case insensitive during migration

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
